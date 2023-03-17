bcd59f41cc16bef84c52577c6d797bf729711cd8
fahmi-aldairi <fahmialdairi10@gmail.com>
2023-03-17 22:26:05 +0300

    End the Task

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,30 @@
+let visitor = prompt("Hello, Please enter your name :");
+let gender = prompt("Can you tell us your gender?");
+let lowerGender = gender.toLowerCase();
 
 
-let visitor = prompt("Please enter your name:");
-let gender = prompt("what is your gender?");
-let lowerGender = gender.toLocaleLowerCase();
-
-if(lowerGender === "male"){
-    alert("Welcome Mr."+visitor);
-}
-else if( lowerGender === "female"){
-    alert("Welcome Ms."+visitor);
-}
-else{
-    alert("Welcome "+visitor);
+if (lowerGender === "male") {
+  alert("Welcome" + " Mr." + visitor);
+  let drink = prompt("Do you prefer a hot or cold drink Mr. " + visitor + "?");
+  let nameDrink = prompt(
+    "Please, write the name of your drink Mr. " + visitor + "?"
+  );
+  let gettingPrepared = alert("Your drink is getting prepared Mr. " + visitor);
+  console.log(nameDrink + " for Mr." + visitor);
+} else if (lowerGender === "female") {
+  alert("Welcome Ms." + visitor);
+  let drink = prompt("Do you prefer a hot or cold drink Ms. " + visitor + "?");
+  let nameDrink = prompt(
+    "Please, write the name of your drink Ms. " + visitor + "?"
+  );
+  let gettingPrepared = alert("Your drink is getting prepared Ms. " + visitor);
+  console.log(nameDrink + " for Ms." + visitor);
+} else {
+  alert("Welcome " + visitor);
+  let drink = prompt("Do you prefer a hot or cold drink " + visitor + "?");
+  let nameDrink = prompt(
+    "Please, write the name of your drink " + visitor + "?"
+  );
+  let gettingPrepared = alert("Your drink is getting prepared " + visitor);
+  console.log(nameDrink + " for " + visitor);
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Caffe--Z
+
+**It's a caffee Website**
+
+### Used Tools
+- VS.Code
+- HTML
+- Bootstrap
+- Css
+- JavaScript
+
+[**live code**]()
