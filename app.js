1cebd1e8f84daa47a9c244dfca6afd6b39492fb5
fahmi-aldairi <fahmialdairi10@gmail.com>
2023-03-21 16:32:39 +0300

    solve task3

--- app.js
+++ app.js
@@ -69,39 +69,87 @@
 
 /////////// Task-3 //////////
 
-let visitor = prompt("Hello,please type your name?");
-let drinkType, drinkName;
-let gender = prompt("Tell us your Gender!!").toLowerCase();
+// let visitor = prompt("Hello,please type your name?");
+// let drinkType, drinkName;
+// let gender = prompt("Tell us your Gender!!").toLowerCase();
 
+// function gen() {
+//   while (gender != "male" && gender != "female") {
+//     gender = prompt("Type your gender correctly,please.");
+//   }
+//   if (gender === "male") {
+//     alert("Welcome" + " Mr." + visitor);
+//     drinkType = prompt(
+//       "Do you prefer a hot or cold drink Mr. " + visitor + "?"
+//     );
+//     drinkName = prompt(
+//       "Please, write the name of your drink Mr. " + visitor + "?"
+//     );
+//     alert("Your drink is getting prepared Mr. " + visitor);
+//   } else if (gender === "female") {
+//     alert("Welcome" + " Ms." + visitor);
+//     drinkType = prompt(
+//       "Do you prefer a hot or cold drink Ms. " + visitor + "?"
+//     );
+//     drinkName = prompt(
+//       "Please, write the name of your drink Ms. " + visitor + "?"
+//     );
+//     alert("Your drink is getting prepared Ms. " + visitor);
+//   }
+// }
+
+// let gn = gen();
+
+// let info = [visitor, gender, drinkType, drinkName];
+
+// for (let i = 0; i < info.length; i++) {
+//   console.log(info[i]);
+// }
+
+/////////// Task-4 //////////
+
+let visitor = prompt("Hello, Please enter your name :");
+let gender = prompt("Can you tell us your gender?");
+let lowerGender = gender.toLowerCase();
 function gen() {
-  while (gender != "male" && gender != "female") {
-    gender = prompt("Type your gender correctly,please.");
+  while (lowerGender != "male" && lowerGender != "female") {
+    gender = prompt("Please, select a gender");
+    lowerGender = gender.toLowerCase();
   }
-  if (gender === "male") {
+
+  if (lowerGender === "male") {
     alert("Welcome" + " Mr." + visitor);
-    drinkType = prompt(
-      "Do you prefer a hot or cold drink Mr. " + visitor + "?"
-    );
-    drinkName = prompt(
-      "Please, write the name of your drink Mr. " + visitor + "?"
-    );
-    alert("Your drink is getting prepared Mr. " + visitor);
-  } else if (gender === "female") {
-    alert("Welcome" + " Ms." + visitor);
-    drinkType = prompt(
-      "Do you prefer a hot or cold drink Ms. " + visitor + "?"
-    );
-    drinkName = prompt(
-      "Please, write the name of your drink Ms. " + visitor + "?"
-    );
-    alert("Your drink is getting prepared Ms. " + visitor);
+  } else if (lowerGender === "female") {
+    alert("Welcome Ms." + visitor);
   }
 }
-
 let gn = gen();
 
-let info = [visitor, gender, drinkType, drinkName];
-
+let drink = prompt("Do you prefer a hot or cold drink " + visitor + "?");
+let nameDrink = prompt("Please, write the name of your drink " + visitor + "?");
+let gettingPrepared = alert("Your drink is getting prepared " + visitor);
+let info = [visitor, gender, drink, nameDrink];
 for (let i = 0; i < info.length; i++) {
   console.log(info[i]);
 }
+let visitorInfo = document.getElementById("Visitor_Info");
+let div = document.createElement("div");
+let p = document.createElement("p");
+let ul = document.createElement("ul");
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+
+p.textContent = `${"Name :"} ${visitor}`;
+li1.textContent = `${"Gender :"} ${gender}`;
+li2.textContent = `${"Drink:"} ${drink}`;
+li3.textContent = `${"Drink type:"} ${nameDrink}`;
+
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+div.appendChild(p);
+div.appendChild(ul);
+
+visitorInfo.appendChild(div);
